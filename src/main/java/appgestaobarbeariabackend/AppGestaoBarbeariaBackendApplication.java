c1a024147297a3c354b23b3b1b8cd77fe6fee3a3
pedromiquiles <miquiles.lima@hotmail.com>
2022-04-27 12:32:38 -0300

    firtst

---
+++ src/main/java/appgestaobarbeariabackend/AppGestaoBarbeariaBackendApplication.java
@@ -0,0 +1,13 @@
+package appgestaobarbeariabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGestaoBarbeariaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGestaoBarbeariaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/appgestaobarbeariabackend/AppGestaoBarbeariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package appgestaobarbeariabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGestaoBarbeariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
